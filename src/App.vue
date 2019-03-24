da21c984d50ef3d403c5cbb535c332fdcb972889
MOUSSAOUI Ahmed <k.mame2810@gmail.com>
2019-03-24 17:19:32 +0100

    comp: navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar/>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {};
+import Navbar from "@/components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+};
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="home">
+    <h1>This is Home !</h1>
     <img alt="Vue logo" src="../assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>--->
-    <Alert type="danger" message="Hey nibba !"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-import Alert from "@/components/Alert.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld,
-    Alert
-  }
+  name: "home"
 };
 </script>
